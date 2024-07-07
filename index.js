738647befac9945662c4bde2f521a1077c64e6a6
ShivanshAr97 <shivansh.arora973@gmail.com>
2024-07-08 02:30:48 +0530

    commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 
 const corsOptions ={
-    origin:['http://localhost:5173', 'https://accredian-frontend-cas3kecno-shivanshar97s-projects.vercel.app'], 
+    origin:['http://localhost:5173', 'https://accredian-frontend-sage.vercel.app/'], 
     credentials:true,
     optionSuccessStatus:200
   }
